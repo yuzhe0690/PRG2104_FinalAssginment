bf39b043ff66fcf5f53b18c96b52e763c970d395
yuzhe0690 <chengycchong@gmail.com>
2023-10-09 15:39:31 +0800

    Create SnakeBody.scala
    + children class of the cell parent class.
    + Used for the Snake class to have a list to store the list of the body.

---
+++ src/main/scala/ch/makery/address/model/SnakeBody.scala
@@ -0,0 +1,7 @@
+package ch.makery.address.model
+
+import scalafx.scene.paint.Color
+
+class SnakeBody(x: Double, y: Double) extends Cell(x, y) {
+  color = Color.DarkCyan // set the color of the snake's body
+}
